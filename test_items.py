2ec94e945c8a4e5df6fe49e4f702dde7b79289e1
Traismer <98160738+Traismer@users.noreply.github.com>
2022-12-30 00:35:34 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 import time
+import pytest
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
@@ -12,5 +13,7 @@ def test_get_order_button(driver):
     button_order = driver.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
     
     assert button_order, 'Button order not found'
-        
 
+
+if __name__ == '__main__':
+    pytest.main()
