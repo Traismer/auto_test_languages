0fb0bc614d8cfd9eb26525d9e75d5227368ac06c
Traismer <98160738+Traismer@users.noreply.github.com>
2022-12-30 00:13:05 +0300

    Add files via upload
    add file

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser', action='store', default='chrome',
+                     help='Choose browser: chrome or firefox(waiting for the bug fix)')
+    parser.addoption('--language', action='store', default="ru",    # default=None
+                     help="Choose language: '--language=en' or '--language=ru'")
+
+
+# pytest -s -v --browser=chrome --language=ru test_items.py
+@pytest.fixture(scope='function')
+def driver(request):
+    browser = request.config.getoption('browser')
+    user_language = request.config.getoption("language")
+    match browser:
+        case 'chrome':
+            print('\nstart chrome browser for test..')
+            options = Options()
+            options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+
+            driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+            driver.maximize_window()
+        # case 'firefox':   # waiting for the bug fix driver firefox 28.12.2022
+        #     driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        case _:
+            raise pytest.UsageError('--browser should be chrome or firefox(waiting for the bug fix)')
+    yield driver
+    print('\nquit browser..')
+    driver.quit()
+
---
+++ test_items.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+# 3. должен объявляться в фикстуре "browser" у меня "driver", т.к. я использую match case проверку, спасибо
+def test_get_order_button(driver):
+    driver.get(link)
+    time.sleep(5) # для проверки на --language=fr кнопку "Ajouter au panier"
+    button_order = driver.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
+    assert button_order, 'Button order not found'
+        
+
