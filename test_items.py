94b24e1a9611a41085274f0e7cbb0651313f6d27
Traismer <98160738+Traismer@users.noreply.github.com>
2022-12-30 00:29:27 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,9 +6,11 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 # 3. должен объявляться в фикстуре "browser" у меня "driver", т.к. я использую match case проверку, спасибо
 def test_get_order_button(driver):
+    
     driver.get(link)
     time.sleep(5) # для проверки на --language=fr кнопку "Ajouter au panier"
     button_order = driver.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
+    
     assert button_order, 'Button order not found'
         
 
